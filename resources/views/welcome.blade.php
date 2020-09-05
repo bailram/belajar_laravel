8f76f995b10c8245c553b99d24fa7d1a34d0eb9d
Dhimas Bayu Ilham Ramadhan <dhimasbayu.9f@gmail.com>
2020-09-05 15:22:56 +0700

    add practice routing file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Bailram
                 </div>
 
                 <div class="links">
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('halo', function(){
+    return "Halo, ini merupakan praktik belajar route baru dari Bailram";
+});
+
+Route::get('blog', function(){
+    return view('blog');
+});
